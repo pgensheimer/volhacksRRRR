6e298fc79930a76d0f0d6886f025a76f36b8b10d
pgensheimer <rxz142@vols.utk.edu>
2018-09-29 12:25:58 -0400

    Added entire functionality of the script

---
+++ rscript.py
@@ -0,0 +1,24 @@
+#r->R
+import fileinput
+
+#this function replaces lowercases r with capital Rs
+def rReplace(filein):
+    output = filein.replace("r","R")
+    return output
+
+
+
+if __name__ == "__main__":
+    fileR = input("Please enter file: ")
+    rfile = "R" + fileR
+    writer = open(rfile, "w")
+    reader = open(fileR, "r")
+    
+    filein = reader.read()
+    
+    nl = rReplace(filein)
+    
+    writer.write(nl)
+    
+    writer.close()
+    reader.close()
